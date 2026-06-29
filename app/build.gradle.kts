b3762938822a55423cb39e029055c7fd3097d682
Andrei2012GT9 <andrei10.07.2012popescu@gmail.com>
2026-06-29 18:14:53 +0200

    Bump version to v0.2.0-beta2 and stabilize Material 3 core build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.current.hub"
         minSdk = 31
         targetSdk = 37
-        versionCode = 1
+        versionCode = 2
         versionName = "0.2.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
